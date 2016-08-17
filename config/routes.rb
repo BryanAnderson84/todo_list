653498aeea18b788b7c672364890b8f5d8b43200
Bryan Anderson <bryannobleanderson@live.com>
2016-08-16 19:11:58 -0600

    added edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-resources :tasks
+  root 'static_pages#home'
+
+  get '/about', to: 'static_pages#about', as: "about"
+
+  resources :tasks
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
